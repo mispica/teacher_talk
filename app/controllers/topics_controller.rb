d27138a2accb68a40fe9293cb93a9ad869999afa
mispica <home_3373@yahoo.co.jp>
2023-06-10 20:56:00 +0900

    トピック詳細表示・編集・削除機能実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   def index
     @topics = Topic.all
@@ -18,14 +19,41 @@ class TopicsController < ApplicationController
     end
   end
 
-  def show   
+  def show
+
   end
 
+  def edit
+    if current_user.id != @topic.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @topic.update_attributes(topic_params)
+      redirect_to topic_path(@topic.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if current_user.id == @topic.user_id
+       @topic.destroy
+       redirect_to root_path
+    else
+      render :show
+    end
+  end
 
   private
 
   def topic_params
     params.require(:topic).permit(:topic_title, :topic_content).merge(user_id: current_user.id)
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
   end
 
 end
